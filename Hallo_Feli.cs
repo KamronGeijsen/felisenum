2205b88a4994c2e904d949c3ef5a20acfef484ee
KamronGeijsen <kamrongeijsen@gmail.com>
2015-11-06 20:08:08 +0100

    Update Hallo_Feli.cs

--- Hallo_Feli.cs
+++ Hallo_Feli.cs
@@ -1,12 +1,9 @@
 // Hallo Felisenum in C#, Compile met VS of mono
 using System;
 
-public class Felisenum
-{
-   public static void Main()
-   {
-      for (x=0 ; x< 10 ; x++)
-      {
+public class Felisenum{
+   public static void Main(){
+      for (x=0 ; x< 10 ; x++){
         Console.WriteLine("Hallo Felisenum!");
       }
    }
